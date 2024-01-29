5bc537dae9e927c68c9f6e091e4aa5a3701acc76
DevBobcorn <devbobcorn@outlook.com>
2024-01-29 20:07:08 +0800

    :wrench: Tweak block AO values

--- Runtime/Scripts/Geometry/BlockGeometry.cs
+++ Runtime/Scripts/Geometry/BlockGeometry.cs
@@ -75,8 +75,8 @@ namespace CraftSharp.Resource
 
         private float GetCornerAO(bool side1, bool corner, bool side2)
         {
-            //return 1F - (side1 ? 0.33F : 0F) - (corner ? 0.33F : 0F) - (side2 ? 0.33F : 0F);
-            return 1F - (side1 ? 0.2F : 0F) - (corner ? 0.2F : 0F) - (side2 ? 0.2F : 0F);
+            return 1F - (side1 ? 0.33F : 0F) - (corner ? 0.33F : 0F) - (side2 ? 0.33F : 0F);
+            //return 1F - (side1 ? 0.2F : 0F) - (corner ? 0.2F : 0F) - (side2 ? 0.2F : 0F);
         }
 
         // tl tm tr
