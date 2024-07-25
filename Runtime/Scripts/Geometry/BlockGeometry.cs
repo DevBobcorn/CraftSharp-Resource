69d1b2bf4fb5f47345697918556eda60f8964efa
Tasuku <devbobcorn@outlook.com>
2024-07-25 13:47:09 +0800

    :bug: Fix thickness calculation

--- Runtime/Scripts/Geometry/BlockGeometry.cs
+++ Runtime/Scripts/Geometry/BlockGeometry.cs
@@ -75,6 +75,11 @@ namespace CraftSharp.Resource
             );
         }
 
+        private float GetCornerThickness(bool side1, bool corner, bool side2)
+        {
+            return math.saturate(0.8F - (side1 ? 0.33F : 0F) - (corner ? 0.33F : 0F) - (side2 ? 0.33F : 0F));
+        }
+
         private float GetCornerAO(bool side1, bool corner, bool side2)
         {
             return math.saturate(1F - (side1 ? 0.5F : 0F) - (corner ? 0.5F : 0F) - (side2 ? 0.5F : 0F));
@@ -87,10 +92,10 @@ namespace CraftSharp.Resource
         {
             return new float[]
             {
-                GetCornerAO(!ml, !tl, !tm), // tl
-                GetCornerAO(!tm, !tr, !mr), // tr
-                GetCornerAO(!bm, !bl, !ml), // bl
-                GetCornerAO(!mr, !br, !bm), // br
+                GetCornerThickness(!bm, !br, !mr), // bl
+                GetCornerThickness(!mr, !tr, !tm), // tl
+                GetCornerThickness(!ml, !bl, !bm), // br
+                GetCornerThickness(!tm, !tl, !ml), // tr
             };
         }
 
@@ -112,7 +117,7 @@ namespace CraftSharp.Resource
         private static readonly float[] FULL_THICKNESS = new float[] { 1F, 1F, 1F, 1F };
 
         /// <summary>
-        /// Get 4 corner AO values a face
+        /// Get 4 corner AO values for a face
         /// </summary>
         public float[] GetDirCornersAO(CullDir dir, bool[] castAO)
         {
@@ -134,9 +139,9 @@ namespace CraftSharp.Resource
                     //  6  7  8    o--> unity z+ (East)
                     return GetCornersAO(castAO[24], castAO[25], castAO[26], castAO[15], castAO[17], castAO[ 6], castAO[ 7], castAO[ 8]);
                 case CullDir.NORTH:
-                    //  2 11 20    A unity y+ (Up)
+                    //  2 11 20    A unity z+ (East)
                     //  1 10 19    |
-                    //  0  9 18    o--> unity z+ (East)
+                    //  0  9 18    o--> unity y+ (Up)
                     return GetCornersAO(castAO[ 2], castAO[11], castAO[20], castAO[ 1], castAO[19], castAO[ 0], castAO[ 9], castAO[18]);
                 case CullDir.EAST:
                     //  8 17 26    A unity x+ (South)
@@ -155,42 +160,42 @@ namespace CraftSharp.Resource
         }
 
         /// <summary>
-        /// Get 4 corner thickness values a face (Get AO from inside of the cube)
+        /// Get 4 corner thickness values for a face (Get AO from inside of the cube)
         /// </summary>
         public float[] GetDirCornersThickness(CullDir dir, bool[] castAO)
         {
             switch (dir)
             {
                 case CullDir.DOWN:
-                    //  6  7  8    A unity x+ (South)
-                    //  3  4  5    |
-                    //  0  1  2    o--> unity z+ (East)
-                    return GetCornersThickness(castAO[ 6], castAO[ 7], castAO[ 8], castAO[ 3], castAO[ 5], castAO[ 0], castAO[ 1], castAO[ 2]);
+                    // 11 14 17    A unity z+ (East)
+                    // 10 [] 16    |
+                    //  9 12 15    o--> unity x+ (South)
+                    return GetCornersThickness(castAO[11], castAO[14], castAO[17], castAO[10], castAO[16], castAO[ 9], castAO[12], castAO[15]);
                 case CullDir.UP:
-                    // 20 23 26    A unity z+ (East)
-                    // 19 22 25    |
-                    // 18 21 24    o--> unity x+ (South)
-                    return GetCornersThickness(castAO[20], castAO[23], castAO[26], castAO[19], castAO[25], castAO[18], castAO[21], castAO[24]);
+                    // 15 16 17    A unity x+ (South)
+                    // 12 [] 14    |
+                    //  9 10 11    o--> unity z+ (East)
+                    return GetCornersThickness(castAO[15], castAO[16], castAO[17], castAO[12], castAO[14], castAO[ 9], castAO[10], castAO[11]);
                 case CullDir.SOUTH:
-                    // 24 25 26    A unity y+ (Up)
-                    // 15 16 17    |
-                    //  6  7  8    o--> unity z+ (East)
-                    return GetCornersThickness(castAO[24], castAO[25], castAO[26], castAO[15], castAO[17], castAO[ 6], castAO[ 7], castAO[ 8]);
+                    //  5 14 23    A unity z+ (East)
+                    //  4 [] 22    |
+                    //  3 12 21    o--> unity y+ (Up)
+                    return GetCornersThickness(castAO[ 5], castAO[14], castAO[23], castAO[ 4], castAO[22], castAO[ 3], castAO[12], castAO[21]);
                 case CullDir.NORTH:
-                    //  2 11 20    A unity y+ (Up)
-                    //  1 10 19    |
-                    //  0  9 18    o--> unity z+ (East)
-                    return GetCornersThickness(castAO[ 2], castAO[11], castAO[20], castAO[ 1], castAO[19], castAO[ 0], castAO[ 9], castAO[18]);
+                    // 21 22 23    A unity y+ (Up)
+                    // 12 [] 14    |
+                    //  3  4  5    o--> unity z+ (East)
+                    return GetCornersThickness(castAO[21], castAO[22], castAO[23], castAO[12], castAO[14], castAO[ 3], castAO[ 4], castAO[ 5]);
                 case CullDir.EAST:
-                    //  8 17 26    A unity x+ (South)
-                    //  5 14 23    |
-                    //  2 11 20    o--> unity y+ (Up)
-                    return GetCornersThickness(castAO[ 8], castAO[17], castAO[26], castAO[ 5], castAO[23], castAO[ 2], castAO[11], castAO[20]);
+                    // 19 22 25    A unity y+ (Up)
+                    // 10 [] 16    |
+                    //  1  4  7    o--> unity x+ (South)
+                    return GetCornersThickness(castAO[19], castAO[22], castAO[25], castAO[10], castAO[16], castAO[ 1], castAO[ 4], castAO[ 7]);
                 case CullDir.WEST:
-                    // 18 21 24    A unity y+ (Up)
-                    //  9 12 15    |
-                    //  0  3  6    o--> unity x+ (South)
-                    return GetCornersThickness(castAO[18], castAO[21], castAO[24], castAO[ 9], castAO[15], castAO[ 0], castAO[ 3], castAO[ 6]);
+                    //  7 16 25    A unity x+ (South)
+                    //  4 [] 22    |
+                    //  1 10 19    o--> unity y+ (Up)
+                    return GetCornersThickness(castAO[ 7], castAO[16], castAO[25], castAO[ 4], castAO[22], castAO[ 1], castAO[10], castAO[19]);
 
                 default:
                     return FULL_THICKNESS;
